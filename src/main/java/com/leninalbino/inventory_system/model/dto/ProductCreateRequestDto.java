9afcf8bef1339f03b8b3055e29a6e67bd30e66d0
Lenin Albino <leninalbino@gmail.com>
2025-08-21 18:44:28 -0500

    add JSON property annotations to ProductCreateRequestDto fields

--- src/main/java/com/leninalbino/inventory_system/model/dto/ProductCreateRequestDto.java
+++ src/main/java/com/leninalbino/inventory_system/model/dto/ProductCreateRequestDto.java
@@ -1,13 +1,15 @@
 package com.leninalbino.inventory_system.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class ProductCreateRequestDto {
+    @JsonProperty("name")
     private String productName;
     private String description;
-    private String price; // Recibido como String desde el frontend
+    private String price;
     private Integer quantity;
+    @JsonProperty("category")
     private Long categoryId;
 }
-
